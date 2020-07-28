bad0e8465e5bb59d74052452d7895163012c5541
Victoria Smith <victoriasmithh428@gmail.com>
2020-07-28 17:04:08 -0400

    validate number

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector('#generate')
+let lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ'
+let num = '1234567890'
+let symbol = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let charLength = parseInt(
+    prompt(
+      'How many characters would you like your password to be? Please pick a number between 8 and 128.'
+    )
+  )
+  if (charLength >= 8 && charLength <= 128) {
+    let containsLower = confirm('Would you like lower case characters?')
+    let containsUpper = confirm('Would you like upper case characters?')
+    let containsNum = confirm('Would you like numbers?')
+    let containsSym = confirm('Would you like symbols?')
+  } else {
+    alert('Invalid input')
+  }
 
-  passwordText.value = password;
+  console.log({
+    charLength,
+    containsLower,
+    containsUpper,
+    containsNum,
+    containsSym,
+  })
+  let password = generatePassword()
+  let passwordText = document.querySelector('#password')
 
+  passwordText.value = password
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword)
